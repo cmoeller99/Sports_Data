3cbb6d71b25145844b096089630e7f7e84a6fa4b
Colby <colbymoeller@gmail.com>
2021-10-22 20:16:10 -0700

    Update Sports_Stats.py

--- Sports_Stats.py
+++ Sports_Stats.py
@@ -1,1 +1,13 @@
-print("return Sports_Data")
+from nba_api.stats.static import teams
+
+### Ask for the sport
+Sports = input("please enter a Sport")
+
+
+'''
+# get_teams returns a list of 30 dictionaries, each an NBA team.
+nba_teams = teams.get_teams()
+print('Number of teams fetched: {}'.format(len(nba_teams)))
+nba_teams[:3]
+
+'''
